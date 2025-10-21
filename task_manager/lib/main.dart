bf41895e8d74eec3bb8d324f6c3c43f120982535
Samuel Berhan <berhansami10@gmail.com>
2025-10-21 17:45:51 -0400

    finished main

--- task_manager/lib/main.dart
+++ task_manager/lib/main.dart
@@ -1,25 +1,78 @@
-
 import 'package:flutter/material.dart';
 import 'screens/task_list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const TaskManagerApp());
 }
 
-
-
-class TaskManagerApp extends StatelessWidget{
+class TaskManagerApp extends StatefulWidget {
   const TaskManagerApp({super.key});
 
   @override
-  Widget build (BuildContext context){
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadThemeMode();
+  }
+
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool('isDarkMode');
+    if (isDark != null) {
+      setState(() {
+        _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+      });
+    }
+  }
+
+  Future<void> _toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+    await prefs.setBool('isDarkMode', _themeMode == ThemeMode.dark);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task Manager',
+      themeMode: _themeMode,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
         useMaterial3: true,
+        cardTheme: CardTheme(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
       ),
-      home: const TaskListScreen(),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        cardTheme: CardTheme(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+      home: TaskListScreen(onToggleTheme: _toggleTheme, themeMode: _themeMode),
       debugShowCheckedModeBanner: false,
     );
   }
