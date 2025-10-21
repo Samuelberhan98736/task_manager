513ba9daafa12d877347b534211075c4fcf7628c
Samuel Berhan <berhansami10@gmail.com>
2025-10-21 17:18:51 -0400

    commit

--- task_manager/lib/screens/task_list_screeen.dart
+++ task_manager/lib/screens/task_list_screeen.dart
@@ -1,65 +1,48 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
-import 'models/items.dart';
+import 'package: flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'models/task.dart';
 
-class DBHelper {
-  DBHelper._privateConstructor();
-  static final DBHelper instance = DBHelper._privateConstructor();
 
-  static Database? _database;
+class TaskListScreeen extends StatefulWidget{
+  const TaskListScreeen ({super.key});
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initializeDatabase();
-    return _database!;
-  }
-
-  Future<Database> _initializeDatabase() async {
-    String path = join(await getDatabasesPath(), 'my_database.db');
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: _createTable,
-    );
-  }
-
-  Future<void> _createTable(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE items (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        description TEXT
-      )
-    ''');
-  }
-
-  Future<int> insertItem(Item item) async {
-    final db = await database;
-    return await db.insert('items', item.toMap());
-  }
-
-  Future<List<Item>> getItems() async {
-    final db = await database;
-    final List<Map<String, dynamic>> maps = await db.query('items');
-    return List.generate(maps.length, (i) => Item.fromMap(maps[i]));
-  }
-
-  Future<int> updateItem(Item item) async {
-    final db = await database;
-    return await db.update(
-      'items',
-      item.toMap(),
-      where: 'id = ?',
-      whereArgs: [item.id],
-    );
-  }
-
-  Future<int> deleteItem(int id) async {
-    final db = await database;
-    return await db.delete(
-      'items',
-      where: 'id = ?',
-      whereArgs: [id],
-    );
-  }
+  @override
+  State<TaskListScreeen> create() => _TaskListScreenState();
 }
+
+
+class _TaskListScreenState extends State<TaskListScreen>{
+  //list to store all tasks
+
+  final List<Task> _task = [];
+
+  // controller for the text input
+
+  final TextEditingController _taskController = TextEditingController();
+
+
+  //method to add a new task
+
+  void _addTask(){
+    if(_taskController.text.trim().isnotEmpty){
+      setState((){
+        _tasks.add(Task(name: _taskController.text.trim()));
+        _taskController.clear();
+      });
+    }
+  }
+
+
+  // method to toggle task completion status
+
+  void _toggleTaskCompletion(int index){
+    setState(() {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+
+  
+
+
+}
+
